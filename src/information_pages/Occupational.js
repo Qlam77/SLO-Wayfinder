2b1332f97f7777ce8dcb5c98ebecb0fa1bddc1cd
qlam77 <qlam3@my.bcit.ca>
2017-10-08 16:01:38 -0700

    added additional components for information pages, applied it to occupation for now

--- src/information_pages/Occupational.js
+++ src/information_pages/Occupational.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import InfoHead from './infoResources/InfoHead';
+import InfoBody from './infoResources/InfoBody';
+import InfoLinks from './infoResources/InfoLinks';
 
 class Occupational extends React.Component{
     render() {
         return (
             //your content goes here
-            <p>You are an occupational being</p>
+            <div>
+                <InfoHead>Your Headers will be here</InfoHead>
+                <InfoBody>Your information will show up here</InfoBody>
+                <InfoLinks>Your Links will show up here</InfoLinks>
+            </div>
         )
     }
 };
