8fa3a55b7fdab6712c5121b217addeb4f046a6c9
cjchiang <chonjou@gmail.com>
2017-11-21 18:00:28 -0800

    added react-parallax

--- src/components/DesktopHeader.js
+++ src/components/DesktopHeader.js
@@ -1,15 +1,9 @@
 import React from 'react';
-
+import {Parallax} from 'react-parallax';
 class DesktopHeader extends React.Component {
     render() {
-        const imageSizer = {
-            width: "200px",
-            height: "200px",
-        };
         return (
-            <div className="backgroundHeader">
-                <img style={imageSizer} src="images/bcit_cmyk_large.jpg" alt="bcit"/>
-            </div>
+          <Parallax bgImage=""></Parallax>
         );
     }
 }
