8482fc1348f610332e7453214a2f59276b9dde47
Chonjou Chiang <chonjou@gmail.com>
2017-11-28 11:51:47 -0800

    fied the merge conflict

--- src/App.js
+++ src/App.js
@@ -22,38 +22,6 @@ class App extends React.Component {
         }
     };
 
-    componentDidMount() {
-        const previousList = this.state.listOfLocations;
-        const dimensionList = this.state.categoryList;
-        const rootRef = this.props.db.database().ref().child("1");
-        const childRef = rootRef.child("Services");
-        const rootRef2 = this.props.db.database().ref().child("0");
-        //look for element called dimensions
-        const childRef2 = rootRef2.child("Dimensions");
-        //get all current values at the current location
-        childRef2.once('value', snap1 => {
-            //iterate over each of these elements
-            snap1.forEach((childSnapshot1) => {
-                //add each element to the local array
-                // childRef.child(childSnapshot.key)
-                dimensionList.push({
-                    dimensionName: childSnapshot1.key,
-                });
-            });
-        });
-
-
-        childRef.on('child_added', snap => {
-            previousList.push({
-                serviceWebName: snap.val().Name.replace(/\s/g,''),
-                serviceName: snap.key
-            });
-            this.setState({
-                listOfLocations: previousList
-            });
-        });
-    }
-
     componentWillMount() {
         window.addEventListener('resize', this.handleWindowSizeChange);
     }
